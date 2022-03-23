c03ce0f430a679bda05f57e14427489c434708e6
Star Yellow <1147036991@qq.com>
2022-03-23 15:01:56 +0800

    v1.7

--- Assets/MusicPlayerMaker/Editor/MusicPlayerGenerator.cs
+++ Assets/MusicPlayerMaker/Editor/MusicPlayerGenerator.cs
@@ -20,11 +20,10 @@ namespace MusicPlayerMaker {
             GetWindow<MusicPlayerMakerWindow>("MusicPlayerMaker");
         }
 
-        //[SerializeField]
-        //AudioClip music01;
-        UdonBehaviour udon;
-
-
+        [SerializeField]
+        public UdonBehaviour udon;
+        [SerializeField]
+        Texture2D Image;
         [SerializeField]
         GameObject PlayerObject;
         [SerializeField]
@@ -35,9 +34,12 @@ namespace MusicPlayerMaker {
         Vector2 ButtonSize = new Vector2(170, 24);
         [SerializeField]
         float ButtonMargin = 5;
+        [SerializeField]
+        float ButtonA =0.4f;
         public MusicPlayer muc;
        public  AudioClip[] audio;
         public AudioClip[] lsau;
+
         int zong;
         int lg;
         bool cz=false;
@@ -50,6 +52,8 @@ namespace MusicPlayerMaker {
         void OnGUI()
         {
             var oldPlayerObject = PlayerObject;
+            var oldimage = Image;
+            var oldbta = ButtonA;
             //muc = GameObject.Find("MusicPlayer").GetComponent<MusicPlayer>();
             
             //udon.publicVariables.TryGetVariableValue("", out music01);
@@ -60,31 +64,79 @@ namespace MusicPlayerMaker {
 
             if (GUILayout.Button("添加音频"))
                 MakePlayer();
-            if (GUILayout.Button("重新排序和大小修改(删减音频后要确保此按钮已被点击或自动执行）"))
+            if (GUILayout.Button("重新排序和按钮大小修改(删减音频后要确保此按钮已被点击或自动执行）"))
                 Px();
             EditorGUILayout.PropertyField(so.FindProperty("PlayerObject"), new GUIContent("音乐播放器"));
             EditorGUILayout.PropertyField(so.FindProperty("AudioSource"), new GUIContent("AudioSource"));
+            EditorGUILayout.PropertyField(so.FindProperty("Image"), new GUIContent("背景图片"));
+            EditorGUILayout.PropertyField(so.FindProperty("ButtonA"), new GUIContent("歌曲按钮背景不透明度0～1"));
             EditorGUILayout.PropertyField(so.FindProperty("ButtonSize"), new GUIContent("按钮大小"));
             EditorGUILayout.PropertyField(so.FindProperty("ButtonMargin"), new GUIContent("按钮行距"));
             EditorGUILayout.PropertyField(so.FindProperty("AudioClips"), new GUIContent("音频组添加（如果添加或删减后点播放测试发现播放器主脚本里的music组被还原到上一次了（如果有些歌曲点不了可以去看看这里的问题），请先点排序后手动对music组进行展开再合上即可，我也不知道为什么）"), true);
 
             so.ApplyModifiedProperties();
-
+            if(PlayerObject==null)
+            {
+                return;
+            }
+            var canvas = PlayerObject.FindChild("Canvas");
+            var image = canvas.GetOrAddComponent<Image>();
+            var scr = PlayerObject.FindChild("Canvas").FindChild("Scroll View");
+            int js;
+            Transform[] zg;
+            var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
+            zg = content.GetComponentsInChildren<Transform>();
+            js = (zg.Length - 1) / 3;
+            if (js != 0 && PlayerObject != oldPlayerObject && PlayerObject != null)
+            {
+                var bl = PlayerObject.FindChildOrCreate("变量存储");
+                var textOba = bl.FindChildOrCreate("按钮不透明度");
+                ButtonA = textOba.GetOrAddComponent<Text>().text==""? 0.4f : float.Parse(textOba.GetOrAddComponent<Text>().text);
+                var textbtx = bl.FindChildOrCreate("按钮大小x");
+                var textbty = bl.FindChildOrCreate("按钮大小y");
+                ButtonSize = textbtx.GetOrAddComponent<Text>().text == "" && textbty.GetOrAddComponent<Text>().text == "" ? new Vector2(170, 24) : new Vector2(float.Parse(textbtx.GetOrAddComponent<Text>().text), float.Parse(textbty.GetOrAddComponent<Text>().text));
+                var textbm = bl.FindChildOrCreate("按钮行距");
+                ButtonMargin = textbm.GetOrAddComponent<Text>().text == "" ? 5 : float.Parse(textbm.GetOrAddComponent<Text>().text);
+            }
+            if (ButtonA>1)
+            {
+                ButtonA = 1f;
+            }
+            if(ButtonA!=oldbta && PlayerObject != null&& PlayerObject == oldPlayerObject)
+            {
+                Px();
+            }
+            if (Image != null && Image != oldimage && PlayerObject != null)
+            { 
+                upimage();
+            }
+            else if(Image == null&&oldimage!=null)
+            {
+                image.enabled = false;
+                scr.GetComponent<Image>().enabled = true;
+            }
             if (PlayerObject != null && PlayerObject != oldPlayerObject)
             {
-                po = PlayerObject;
+                
+                if(image.sprite != null)
+                {
+                  if (Image == null && canvas.GetComponent<Image>().sprite != null)
+                      Image = canvas.GetComponent<Image>().sprite.texture;
+                }
+                else
+                {
+                    image.enabled = false;                   
+                    scr.GetComponent<Image>().enabled = true;
+                }
                 udon = PlayerObject.GetComponent<UdonBehaviour>();
                 udon.publicVariables.TryGetVariableValue("music", out jc);
                 var obj = PlayerObject.FindChild("Audio Source");
                 if (obj != null)
                     AudioSource = obj.GetComponent<AudioSource>();
+                upimage();
                 if (jc.Length!=0)
                 {
-                    int js;
-                    Transform[] zg;
-                    var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
-                    zg = content.GetComponentsInChildren<Transform>();
-                    js = (zg.Length - 1) / 3;
+                    
                     if(js>jc.Length)
                     {
                         Debug.LogError("发现主脚本MusicPlayer里的music音频组与已经添加在场景上的按钮不符，如果您不想删除的话，请对照左边歌曲名称层级顺序进行重新手动排序处理！");
@@ -100,18 +152,44 @@ namespace MusicPlayerMaker {
             }
         }
 
+        void upimage()
+        {
+            if (Image != null)
+            {
+                var scr = PlayerObject.FindChild("Canvas").FindChild("Scroll View");
+                var canvas = PlayerObject.FindChild("Canvas");
+                var image = canvas.GetComponent<Image>();
+                scr.GetComponent<Image>().enabled = false;
+                image.enabled = true;
+                image.sprite = Sprite.Create(Image, new Rect(0, 0, Image.width, Image.height),new Vector2(0.5f,0.5f));                
+                image.color = new Color(image.color.r, image.color.g, image.color.b, 0.4f);
+                image.type = (Image.Type)3;
+                image.fillMethod = (Image.FillMethod)1;
+                image.fillOrigin = 0;
+                image.fillAmount = 0.9f;
+               
+            }
+        }
         void Px()
         {
+
             if (PlayerObject == null)
                 return;
-            if(cz)
+            upimage();
+            var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
+            Transform[] zg;
+            zg = content.GetComponentsInChildren<Transform>();
+            int js;
+            js = (zg.Length - 1) / 3;
+            if (cz)
             {
+
                 udon.publicVariables.TryGetVariableValue("music", out jc);
-                if(jilu==jc.Length)
+                if(js==jc.Length)
                 {
                     lsau = jc;
                 }
-                else if(jilu!=jc.Length)
+                else if(js!=jc.Length)
                 {
                     Debug.LogError("请先去把主脚本MusicPlayer里的music音频组手动重新排序！");
                     return;
@@ -119,9 +197,7 @@ namespace MusicPlayerMaker {
                 
             }
             cz = false;
-            var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
-            Transform[] zg;
-            zg = content.GetComponentsInChildren<Transform>();
+            
             var lll = 0;
             foreach (Transform child in zg)
             {
@@ -129,11 +205,11 @@ namespace MusicPlayerMaker {
                 {
                     child.gameObject.GetComponent<Text>().text = lll.ToString();
                     lll++;
+                    child.gameObject.GetComponent<Text>().enabled = false;
                 }
             }
             lll = 0;
-            int js;
-            js = (zg.Length - 1) / 3;
+            
             
             int lf= AudioClips != null?AudioClips.Length:0;
             if(lf!=0)
@@ -204,8 +280,9 @@ namespace MusicPlayerMaker {
                         break;
                     }
                 }
-                
 
+                var image = buttonObject.GetComponent<Image>();
+                    image.color = new Color(image.color.r, image.color.g, image.color.b, ButtonA);
                 var rectTransform = buttonObject.GetComponent<RectTransform>();
                 rectTransform.sizeDelta = ButtonSize;
                 rectTransform.localPosition = Vector3.zero;
@@ -213,16 +290,43 @@ namespace MusicPlayerMaker {
                 rectTransform.localRotation = new Quaternion(0, 0, 0, 0);
                 rectTransform.anchoredPosition = new Vector2(0, offset - (i) * (ButtonSize.y + ButtonMargin));
 
+                
+                var textObject = buttonObject.FindChildOrCreate("Text");
+                var text = textObject.GetOrAddComponent<Text>();
+                var textRectTransform = textObject.GetComponent<RectTransform>();
+                textRectTransform.sizeDelta = 5 * ButtonSize;
+                textRectTransform.localPosition = Vector3.zero;
+                textRectTransform.localScale = new Vector3((float)0.2, (float)0.2, 1);
+                textRectTransform.localRotation = new Quaternion(0, 0, 0, 0);
+                textRectTransform.anchoredPosition = Vector2.zero;
+                text.fontSize = (int)(70 * (ButtonSize.y / 24));
+
 
 
             }
+            var bl = PlayerObject.FindChildOrCreate("变量存储");
+            var textOba = bl.FindChildOrCreate("按钮不透明度");
+            textOba.GetOrAddComponent<Text>().text = ButtonA.ToString();
+            textOba.GetOrAddComponent<Text>().enabled = false;
+            var textbtx = bl.FindChildOrCreate("按钮大小x");
+            textbtx.GetOrAddComponent<Text>().text = ButtonSize.x.ToString();
+            textbtx.GetOrAddComponent<Text>().enabled = false;
+            var textbty = bl.FindChildOrCreate("按钮大小y");
+            textbty.GetOrAddComponent<Text>().text = ButtonSize.y.ToString();
+            textbty.GetOrAddComponent<Text>().enabled = false;
+            var textbm = bl.FindChildOrCreate("按钮行距");
+            textbm.GetOrAddComponent<Text>().text = ButtonMargin.ToString();
+            textbm.GetOrAddComponent<Text>().enabled = false;
             lsau = new AudioClip[lg];
             Array.Copy(audio, lsau, audio.Length);
+            Selection.activeObject = udon;
             udon.publicVariables.TrySetVariableValue("music", audio);
             udon.SetProgramVariable("music", audio);
+            Selection.activeObject = udon;
             Debug.Log("排序完成");
 
         }
+
 
         void MakePlayer()
         {
@@ -247,23 +351,25 @@ namespace MusicPlayerMaker {
             //Enumerable.Range(0, stopButton.onClick.GetPersistentEventCount()).Reverse().ToList().ForEach(i => UnityEventTools.RemovePersistentListener(stopButton.onClick, i));
             //UnityEventTools.AddVoidPersistentListener(stopButton.onClick, stopDelegate);
             }
+            var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
+            Transform[] zg;
+            int js, ss;
+            zg = content.GetComponentsInChildren<Transform>();
+            js = (zg.Length - 1) / 3;
             if (cz)
             {
                 Px();
                 udon.publicVariables.TryGetVariableValue("music", out jc);
-                if (jilu != jc.Length)
+                if (js != jc.Length)
                 {
                     return;
                 }
 
             }
             cz = false;
-            var content = PlayerObject.FindChild("Canvas").FindChild("Scroll View").FindChild("Viewport").FindChild("Content");
-            Transform[] zg;
-            int js, ss;
+           
             ss = 0;
-            zg = content.GetComponentsInChildren<Transform>();
-            js = (zg.Length - 1) / 3;
+            
             int lf = AudioClips != null ? AudioClips.Length : 0;
             bool nu = true;
             if (lf != 0)
@@ -336,7 +442,8 @@ namespace MusicPlayerMaker {
                 audio[i + js - sss] = AudioClips[i];
                 var buttonObject = content.FindChildOrCreate(audioClip.name);
                 
-                var image = buttonObject.GetOrAddComponent<Image>();
+                var image = buttonObject.GetOrAddComponent<Image>();                
+                    image.color=new Color(image.color.r, image.color.g, image.color.b,ButtonA);
                 var button = buttonObject.GetOrAddComponent<Button>();
                 var uuudon = buttonObject.AddUdonSharpComponent<playbutton>();
                 var uudon = buttonObject.GetComponent<UdonBehaviour>();
@@ -363,18 +470,21 @@ namespace MusicPlayerMaker {
                 var textObject = buttonObject.FindChildOrCreate("Text");
                 var text = textObject.GetOrAddComponent<Text>();
                 var textRectTransform = textObject.GetComponent<RectTransform>();
-                textRectTransform.sizeDelta = ButtonSize;
+                textRectTransform.sizeDelta = 5*ButtonSize;
                 textRectTransform.localPosition = Vector3.zero;
-                textRectTransform.localScale = Vector3.one;
+                textRectTransform.localScale = new Vector3((float)0.2, (float)0.2,1);
                 textRectTransform.localRotation = new Quaternion(0, 0, 0, 0);
                 textRectTransform.anchoredPosition = Vector2.zero;
                 text.text = audioClip.name;
+                text.fontSize = (int)(70 *(ButtonSize.y/24));
                 text.color = Color.black;
                 text.alignment = TextAnchor.MiddleCenter;
                 var texts = buttonObject.FindChildOrCreate("Text1");
                 var texts1 = texts.GetOrAddComponent<Text>();
                 texts1.text = (i + js - sss).ToString();
-                
+                texts1.enabled = false;
+
+
             }
             //muc.setaudio(AudioClips);
             lsau = new AudioClip[audio.Length];
@@ -382,8 +492,10 @@ namespace MusicPlayerMaker {
 
             var sb = AudioClips.Length - ss;
             Debug.Log("已添加"+sb+"个对象，共" + lsau.Length+"个对象");
+            Selection.activeObject = udon;
             udon.publicVariables.TrySetVariableValue("music", audio);
             udon.SetProgramVariable("music", audio);
+            Selection.activeObject = udon;
             AudioClips = null;
             Px();
         }
